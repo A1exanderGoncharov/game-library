ce746f3986b6f30fc5d1aa4ca99c3b6b0114d51c
Sanch <sanchesko13@gmail.com>
2023-03-20 00:09:33 +0200

    refactor GenreController

--- Web/Controllers/GenreController.cs
+++ Web/Controllers/GenreController.cs
@@ -1,16 +1,13 @@
 ﻿using BLL.DTO;
 using BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Web.Controllers
 {
     public class GenreController : Controller
     {
-        IGenreService _genreService;
+        readonly IGenreService _genreService;
 
         public GenreController(IGenreService genreService)
         {
@@ -40,8 +37,6 @@ namespace Web.Controllers
                 return RedirectToAction("Index");
             }
             return BadRequest();
-            //var comments = await _genreService.GetAllAsync();
-            //return RedirectToAction("GameDetails", "Game");
         }
 
         public async Task<IActionResult> Delete(int id)

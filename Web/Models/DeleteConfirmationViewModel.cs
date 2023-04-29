34e7ecd22cec35b0a0ed052082a74a2e02851e6e
Sanch <sanchesko13@gmail.com>
2023-04-29 17:24:35 +0300

    add modal window to confirm deletion

---
+++ Web/Models/DeleteConfirmationViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Web.Models
+{
+    public class DeleteConfirmationViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+
+        public DeleteConfirmationViewModel(int id, string name, string controllerName, string actionName)
+        {
+            Id = id;
+            Name = name;
+            ControllerName = controllerName;
+            ActionName = actionName;
+        }
+    }
+}
